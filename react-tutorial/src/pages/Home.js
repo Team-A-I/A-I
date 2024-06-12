d1a3761b6a2e730ff6267a939bb94290455ae869
배정현 <bae4445@gmail.com>
2024-06-12 17:33:01 +0900

    [Feature] 홈화면 개발 1차 완료

--- react-tutorial/src/pages/Home.js
+++ react-tutorial/src/pages/Home.js
@@ -3,13 +3,12 @@ import { Container, Grid, Typography, Button, Box } from '@mui/material';
 import { motion } from 'framer-motion';
 import '../css/Home.css'; // 스타일 파일 임포트
 import katalkImage from '../images/katalk.png'; // 이미지 파일 임포트
-import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone'; 
 
 const Home = () => {
   return (
     <Container maxWidth="lg">
       <Grid container spacing={2} className="home-container">
-        <Grid item xs={12} md={4} className="home-image">
+        <Grid item xs={12} md={6} className="home-image">
           <motion.img 
             src={katalkImage} 
             alt="Pigeon AI" 
@@ -19,27 +18,36 @@ const Home = () => {
             transition={{ type: 'spring', stiffness: 50 }}
           />
         </Grid>
-        <Grid item xs={12} md={8} className="home-text">
+        <Grid item xs={12} md={6} className="home-text">
           <Box>
             <motion.div
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1 }}
             >
-              <Typography variant="header" component="p" gutterBottom>
-                AI Chat Emotion Analyzer  <PhoneIphoneIcon />
+              <Typography variant="header" component="p" gutterBottom className="home-text-header">
+                AI Chat Emotion Analyzer 💑
               </Typography>
               <Typography variant="h3" component="h1" gutterBottom>
                 AI 채팅 감정 분석기
               </Typography>
-              <Typography variant="body1" component="p" gutterBottom>
+              <Typography variant="body1" component="p" gutterBottom style={{ marginTop: '40px' }}>
                 AI기반으로 채팅내용을 분석하여 
               </Typography>
               <Typography variant="body1" component="p" gutterBottom>
                 상대방의 감정을 알아보세요
               </Typography>
-              <Button variant="contained" color="primary" href="#">
-                채팅 업로드 
+              <Button 
+                variant="contained" 
+                color="primary" 
+                href="#" 
+                style={{ 
+                  marginTop: '30px', 
+                  padding: '10px 60px', 
+                  fontSize: '15px'
+                }}
+              >
+                알아보러 가기 
               </Button>
             </motion.div>
           </Box>
