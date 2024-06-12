386502974c01ab9c9271586896837fe5afa790df
배정현 <bae4445@gmail.com>
2024-06-12 12:05:17 +0900

    first commit

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import Home from './pages/Home';
 import Analysis from './pages/Analysis';
-import './css/App.css'; // CSS 파일 임포트
+import './css/App.css';
 
 function App() {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/analysis">Analysis</Link>
-          </li>
-        </ul>
-      </nav>
-
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            A:I
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/analysis">Analysis</Button>
+        </Toolbar>
+      </AppBar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/analysis" element={<Analysis />} />
-        <Route path="/loading" element={<Analysis />} />
       </Routes>
     </div>
   );
